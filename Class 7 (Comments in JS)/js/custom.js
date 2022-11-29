26c24f07875c8bc4baae9c4464f1456750ed3d63
nuzhatmila1997 <nuzhatmila5@gmail.com>
2022-11-29 11:19:06 +0600

    Comments in JavaScript

---
+++ Class 7 (Comments in JS)/js/custom.js
@@ -0,0 +1,19 @@
+// Code to Show Message in console [Start] (this line will not execute)
+console.log("Hi, Mila."); // This is also a valid comment and this statement will get executed
+console.log("This is Class 6.");
+//alert("Comment");
+// Code to Show Message in console [End] (this line will not execute)
+
+/*
+================================================================
+This is a multiline comment for multiple alert to pop up [Start]
+================================================================
+*/
+alert("Hello");
+alert("Welcome to Class 7");
+alert("Today we will learn about JS Comments.")
+/*
+================================================================
+This is a multiline comment for multiple alert to pop up [End]
+================================================================
+*/
