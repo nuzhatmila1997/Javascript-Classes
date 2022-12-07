72663a7efbd452be2d1163c5a6b838f3637bf114
nuzhatmila1997 <nuzhatmila5@gmail.com>
2022-12-07 10:10:13 +0600

    Unary and binary operators intro in JavaScript

---
+++ Class 19 (unary and binary operator)/js/custom.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//unary negation & binary operators
+let negNumber = -5;
+let posNumber = 5;
+let firstNummber = 5;
+let secondNumber = 2;
+let result = posNumber - secondNumber;
+firstNummber = -firstNummber
+console.log(posNumber,negNumber,firstNummber);
+console.log(result);
+
+result = posNumber + firstNummber; // addition
+console.log(result); // 5+(-5) = 0
+
+result = posNumber - secondNumber; // subtraction
+console.log(result);
+
+result = posNumber * secondNumber; // multiplication
+console.log(result); 
+
+result = result / secondNumber; // division
+console.log(result);
+
+result = 7 % 2; // modulas/ remainder
+console.log(result);
+
+result = 7 ** 2; // power/ exponention 7^2 or 7*7
+console.log(result);
+
+result = 5 ** 3; // power/ exponention 5^3 or 5*5*5
+console.log(result);
