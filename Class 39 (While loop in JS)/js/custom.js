c26f9be554a0dc0a4f946486bed1657a8ee026ce
Fariha Nuzhat Majumdar <61647885+nuzhatmila1997@users.noreply.github.com>
2023-01-22 15:43:12 +0600

    while loop in JavaScript

---
+++ Class 39 (While loop in JS)/js/custom.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let counter = 0;
+let counters = 5;
+
+while (counter<5) {
+    console.log(counter);
+    counter++;
+
+}
+
+while (counters>0) {
+    console.log(counters);
+    counters--;
+
+}
